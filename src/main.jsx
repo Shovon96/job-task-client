82990516abafff6520b882cb7953bff8affd6803
Shovon96 <fakhruddinshovon@gmail.com>
2023-12-21 02:35:51 +0600

    nextUi install

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {NextUIProvider} from '@nextui-org/react'
+import MainLayout from './MainComponent/MainLayout';
+import ErrorPage from './ErrorPage/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    // children:[
+    //   {
+    //     path: '/'
+
+    //   }
+    // ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <RouterProvider router={router} />
+    </NextUIProvider>
   </React.StrictMode>,
 )
